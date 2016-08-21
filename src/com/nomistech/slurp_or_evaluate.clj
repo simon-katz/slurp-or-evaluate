49b4c6d032aa58a87acbde6f64a318990a9d44d9
Simon Katz <sk@nomistech.com>
2016-08-21 11:25:42 +0100

    Rename the store directory...
    ...from "_expensive-store" to "_slurp-or-evaluate-store".

--- src/com/nomistech/slurp_or_evaluate.clj
+++ src/com/nomistech/slurp_or_evaluate.clj
@@ -2,10 +2,10 @@
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]))
 
-(def ^:private expensive-store-dir "_expensive-store")
+(def ^:private slurp-or-evaluate-store-dir "_slurp-or-evaluate-store")
 
 (defn ^:private symbol->filename [sym]
-  (str expensive-store-dir
+  (str slurp-or-evaluate-store-dir
        "/"
        (name sym)))
 
